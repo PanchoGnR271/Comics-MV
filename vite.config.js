c9786b2af679d952983ad8730d0541668f63f9ff
PanchoGnR271 <victormvargas01@gmail.com>
2026-09-10 20:45:36 -0600

    ACTUALIZACIÓN DE NOMBRES Y PRUEBA

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'Comics-Mv-React', // 👈 importante
+  base: 'Comics-MV',
   plugins: [react()],
 });
